ec51a2804fda21f7155fde82c6c856e2e2637658
sasha0552 <admin@sasha0552.org>
2024-09-26 09:22:30 +0000

    (flashinfer) Add pyproject.toml

--- .github/actions/build-flashinfer/before_all.sh
+++ .github/actions/build-flashinfer/before_all.sh
@@ -1,10 +1,20 @@
 #!/bin/sh -e
 
+# Make tempfile deterministic
+cat << "EOF" >> $(python3 -c "import tempfile; print(tempfile.__file__)")
+class _RandomNameSequence:
+  i = -1
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    self.i += 1
+    return format(self.i, "08")
+EOF
+
 # Copy sccache from host
 cp /host/opt/hostedtoolcache/sccache/*/*/sccache /usr/bin
 
 # Show cache stats
 sccache --show-stats
-
-# Install dependencies
-python -m pip install setuptools torch wheel
