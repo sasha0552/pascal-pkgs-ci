1dc4cfd3b1c2e69923945e934db76971b77effac
sasha0552 <admin@sasha0552.org>
2024-10-06 17:09:03 +0000

    Use /var/tmp instead of /tmp

--- scripts/sccache_dist_server.sh
+++ scripts/sccache_dist_server.sh
@@ -6,12 +6,12 @@ export SCCACHE_NO_DAEMON=1
 
 # Start sccache-dist server
 cat << EOF | sudo sccache-dist server --config /dev/stdin || true
-cache_dir = "/tmp/toolchains"
+cache_dir = "/var/tmp/toolchains"
 public_addr = "$(tailscale ip --4):10501"
 scheduler_url = "$SCCACHE_DIST_SCHEDULER_URL"
 
 [builder]
-build_dir = "/tmp/build"
+build_dir = "/var/tmp/build"
 bwrap_path = "/usr/bin/bwrap"
 type = "overlay"
 
