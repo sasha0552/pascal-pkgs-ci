aee51e5e588e7ef60e23aa8cadd93ac5abe31522
sasha0552 <admin@sasha0552.org>
2024-10-06 17:09:53 +0000

    Install sccache on host

--- scripts/common_dependencies.sh
+++ scripts/common_dependencies.sh
@@ -9,5 +9,8 @@ sudo apt-get install --yes bubblewrap moreutils
 # Install tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
+# Install sccache
+curl -fssL https://github.com/sasha0552/sccache/releases/latest/download/sccache | sudo install /dev/stdin /usr/bin/sccache
+
 # Install sccache-dist
 curl -fssL https://github.com/sasha0552/sccache/releases/latest/download/sccache-dist | sudo install /dev/stdin /usr/bin/sccache-dist
